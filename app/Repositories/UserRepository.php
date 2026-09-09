eb619dd14b403e02eeeedcb5b4153fc07d7703c2
Inteyaz <inteyazansari322@gmail.com>
2026-09-09 13:15:54 +0530

    implemented  user repos

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\UserRepositoryInterface;
+use App\Models\User;
+
+class UserRepository implements UserRepositoryInterface
+
+{
+  public function findById(int $id): ?User 
+  {
+    return User::find($id);
+  }
+
+  public function findByEmail(string $email): ?User
+  {
+    return User::where('email', $email)->first();
+  }
+
+  public function create(array $data) : User
+
+  {
+    return User::create($data);
+  }
+
+  public function update(User $user , array $data) : User
+  {
+   $user->update($data);
+
+   return $user->fresh();
+  }
+
+  public function delete(User $user) : bool
+  {
+    return $user->delete();
+  }
+
+
+
+}
