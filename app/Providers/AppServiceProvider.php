39a84f5522630569af663c7a64f24e37c4198d94
Inteyaz <inteyazansari322@gmail.com>
2026-09-09 13:17:40 +0530

    bind user interfaces and repositries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Contracts\UserRepositoryInterface;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,10 +11,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
+ public function register(): void
+{
+    $this->app->bind(
+       UserRepositoryInterface::class,
+        UserRepository::class
+    );
+}
 
     /**
      * Bootstrap any application services.
