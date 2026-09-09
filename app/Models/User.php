b7d0a9fb9fd802563cc3e1a44fa75e1f2dcdc66c
Inteyaz <inteyazansari322@gmail.com>
2026-09-09 12:54:02 +0530

    added repo

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,11 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var list<string>
+     * 
+     *
      */
+
+
     protected $fillable = [
         'name',
         'email',
